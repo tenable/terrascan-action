d1c04ff9550f2fc4de0b6c6953109c02bd5f783f
Cesar Rodriguez <cesar@accurics.com>
2022-05-09 14:56:25 -0400

    limiting push to tags

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -5,8 +5,8 @@ set -o nounset
 set -o pipefail
 
 DOCKER_REPO="tenable/terrascan-action"
-#LATEST_TAG=$(git describe --abbrev=0 --tags)
-#LATEST_TAG_SHORT=$(echo "${LATEST_TAG//v}")
+LATEST_TAG=$(git describe --abbrev=0 --tags)
+LATEST_TAG_SHORT=$(echo "${LATEST_TAG//v}")
 
 # Builds image with two tags ( :latest & :<TAG_NAME> )
-docker build -t ${DOCKER_REPO}:latest . #-t ${DOCKER_REPO}:${LATEST_TAG_SHORT} .
+docker build -t ${DOCKER_REPO}:latest -t ${DOCKER_REPO}:${LATEST_TAG_SHORT} .
